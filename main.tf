d07422f0c2333bd2b5e0283edfee5a347fe1ed2a
Guy Carlos Villette <carlosvillette@gmail.com>
2024-07-06 13:51:02 -0400

    Added backend and devops pipeline

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 terraform {
+
+  backend "s3" {
+    bucket = "carlosvillette-statefile-terraform-bucket"
+    dynamodb_table = "state-lock"
+    key = "global/mystatefile/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
